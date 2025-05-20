a0c521dd176a3847685321fdf8dd4307890072d6
Felipe-2833 <felipe.elucidator@gmail.com>
2025-05-20 04:58:23 -0300

    iniciando projeto, adicionando dependencias e estruturando

---
+++ src/test/java/br/com/fiap/mottuGestor/MottuGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mottuGestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MottuGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
